ae20bafeaee9cbb11d81f3d4694bfa7acdf70735
neroist <pmpiga1@gmail.com>
2024-06-16 05:34:42 -0400

    add `logging` module & make nimib logging optional

--- src/nimib/capture.nim
+++ src/nimib/capture.nim
@@ -12,6 +12,9 @@ template captureStdout*(ident: untyped, body: untyped) =
     let (tmpFile, _) = createTempFile("tmp", "")
     let tmpFileFd: FileHandle = tmpFile.getFileHandle() 
     
+    # needs to be present when stdout isn't being written to by `newNbBlock` (-d:nimibNoLog)
+    stdout.flushFile()
+    
     # writing to stdoutFileno now writes to tmpFile
     tmpFileFd.duplicateTo(stdoutFileno)
     
